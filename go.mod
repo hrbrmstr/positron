0cd35565e0bed312cb825770358d776bd14e2a73
hrbrmstr <bob@rud.is>
2023-09-30 13:31:11 -0400

    feat: version check

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21.1
 
 require github.com/blacktop/go-macho v1.1.164
 
-require github.com/blacktop/go-dwarf v1.0.9 // indirect
+require (
+	github.com/Masterminds/semver/v3 v3.2.1
+	github.com/blacktop/go-dwarf v1.0.9 // indirect
+)
--- main.go
+++ main.go
@@ -5,8 +5,33 @@ import (
 	"os"
 	"regexp"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/blacktop/go-macho"
 )
+
+func versionIsSafe(versionStr string) string {
+
+	requiredVersions := []string{"22.3.24", "24.8.3", "25.8.1", "26.2.1"}
+
+	version, err := semver.NewVersion(versionStr)
+	if err != nil {
+		return "❔"
+	}
+
+	for _, reqVersionStr := range requiredVersions {
+		reqVersion, err := semver.NewVersion(reqVersionStr)
+		if err != nil {
+			continue
+		}
+
+		if version.Major() == reqVersion.Major() && !version.LessThan(reqVersion) {
+			return ("🟢")
+		}
+	}
+
+	return ("🔴")
+
+}
 
 func main() {
 
@@ -41,8 +66,10 @@ func main() {
 	matches := pattern.FindAllString(contentStr, -1)
 
 	appPattern := regexp.MustCompile(`\.app.*`)
+	versPattern := regexp.MustCompile(`^.*Electron/`)
 
 	for _, match := range matches {
-		fmt.Println(appPattern.ReplaceAllString(machoPath, ".app")+":", match)
+		versionStr := versPattern.ReplaceAllString(match, "")
+		fmt.Println(appPattern.ReplaceAllString(machoPath, ".app")+":", match, versionIsSafe(versionStr))
 	}
 }
